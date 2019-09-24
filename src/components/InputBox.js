f799a689d8e013e114eb9ee02d660730c721864f
selmanbey <selmannur@yahoo.com>
2019-09-24 20:44:44 +0100

    fixes incorrect indentation

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,66 +1,60 @@
 import React from 'react';
 
 class InputBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: ''
-        }
-
-        this.sendLog = this.sendLog.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+        message: ''
     }
 
-    sendLog(event) {
-      event.preventDefault();
-      if(this.state.message) {
-          this.setState({
-            message: ''
-          });
-          let dt = new Date();
-          fetch('http://0.0.0.0:3500/chatlog', {
-              method: 'post',
-              headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                  content: this.state.message,
-                  username: this.props.currentUser,
-                  time: dt.getHours() + ':' + dt.getMinutes()
-              })
-          }).then((res) => {
-              res.json().then((content) => {
-                  //
-              });
-          }).catch( (err) => {
-              console.log(err);
-          } );
-      };
-    }
+    this.sendLog = this.sendLog.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event) {
-        event.preventDefault();
-        this.setState({message: event.target.value});
-    }
+  sendLog(event) {
+    event.preventDefault();
+    if(this.state.message) {
+      this.setState({ message: '' });
+      let dt = new Date();
+      fetch('http://0.0.0.0:3500/chatlog', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: this.state.message,
+            username: this.props.currentUser,
+            time: dt.getHours() + ':' + dt.getMinutes()
+        })
+      }).then( res => {
+        res.json().then( content => { /* pass */ });
+      }).catch( err => {
+        console.log(err);
+      });
+    };
+  }
 
-    render () {
-        return (
-            <form className="input-form" onSubmit={this.sendLog}>
-                <input
-                  className="chat-input-box"
-                  type="text"
-                  placeholder="Say Something!"
-                  value={this.state.message}
-                  onChange={this.handleChange}/>
-                <button
-                  className="send-button"
-                  type="submit">SEND</button>
-            </form>
-        );
-    }
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({message: event.target.value});
+  }
+
+  render () {
+    return (
+      <form className="input-form" onSubmit={this.sendLog}>
+        <input
+          className="chat-input-box"
+          type="text"
+          placeholder="Say Something!"
+          value={this.state.message}
+          onChange={this.handleChange}/>
+        <button
+          className="send-button"
+          type="submit">SEND</button>
+      </form>
+    );
+  }
 }
 
 export default InputBox;
-
-// POSSIBLE ISSUES: (1) SENDING EMPTY MESSAGES WITH UNINTENDED "ENTER"
